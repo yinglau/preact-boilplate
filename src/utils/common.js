f036d882b81d777145962f50e13b2f9082783c41
winson <501285598@qq.com>
2020-07-27 21:51:14 +0800

    feat: 增加common.js工具函数

---
+++ src/utils/common.js
@@ -0,0 +1,29 @@
+export function subString(str, len) {
+  var newLength = 0;
+  var newStr = "";
+  var chineseRegex = /[^\x00-\xff]/g;
+  var singleChar = "";
+  var strLength = str.replace(chineseRegex, "**").length;
+  if (strLength > len) {
+    for (var i = 0; i < strLength; i++) {
+      singleChar = str.charAt(i).toString();
+      if (singleChar.match(chineseRegex) != null) {
+        newLength += 2;
+      }
+      else {
+        newLength++;
+      }
+      if (newLength > len) {
+        break;
+      }
+      newStr += singleChar;
+    }
+
+    if (strLength > len) {
+      newStr += "...";
+    }
+  } else {
+    newStr = str;
+  }
+  return newStr;
+}
